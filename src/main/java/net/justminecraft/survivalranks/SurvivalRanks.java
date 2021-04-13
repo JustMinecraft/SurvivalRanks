6e9b21ec44affbb806672614f21938e764585b2f
PureGero <>
2021-04-13 09:36:00 +0000

    Add plugin framework

---
+++ src/main/java/net/justminecraft/survivalranks/SurvivalRanks.java
@@ -0,0 +1,12 @@
+package net.justminecraft.survivalranks;
+
+import org.bukkit.plugin.java.*;
+
+public class SurvivalRanks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
